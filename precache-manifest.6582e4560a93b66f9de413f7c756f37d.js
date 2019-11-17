54191989122af60613e7f40fc7229f8d7aa613e6
priaajisn <priaaji.setyanegoro@gmail.com>
2019-11-17 12:04:13 +0700

    Updates

---
+++ precache-manifest.6582e4560a93b66f9de413f7c756f37d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cbc39f5a654615a1a49bc1cec79631",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b4d03a1811ab763cd3c1",
+    "url": "/Robofriends/static/css/main.38385042.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "b4d03a1811ab763cd3c1",
+    "url": "/Robofriends/static/js/main.bafd627e.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
